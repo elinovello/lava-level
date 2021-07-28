dd338bf704fe07193c4474cc2473999727eba2ef
elinovello <novello.imsa@gmail.com>
2021-07-28 15:47:19 -0300

    cambios a la homepage

--- js/script.js
+++ js/script.js
@@ -21,4 +21,19 @@ document.addEventListener("DOMContentLoaded", function () {
          prevScrollpos = currentScrollPos;
     }
 
+    const slideshowImages = document.querySelectorAll(".slideshow")
+    const nextImageDelay = 3000;    
+    let currentImageCounter = 0; 
+
+    slideshowImages[currentImageCounter].style.opacity = 1;
+
+    setInterval(nextImage, nextImageDelay);
+
+    function nextImage() {
+        slideshowImages[currentImageCounter].style.opacity= 0;
+        currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+        slideshowImages[currentImageCounter].style.opacity = 1;
+    }
+
+
 });
